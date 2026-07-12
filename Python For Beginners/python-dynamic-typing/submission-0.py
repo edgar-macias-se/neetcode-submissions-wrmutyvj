c59810315c0fb98500f5f847a1112caac14d126a
Edgar Macias Palmerin <edgarmacias@devcybsec.com>
2026-07-12 15:47:38 -0600

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = .1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "str"
+print(type(variable))
+variable = []
+print(type(variable))
